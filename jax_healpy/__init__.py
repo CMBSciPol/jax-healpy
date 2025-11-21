a0c60d451cdccf75abbd6f7d83f7a6bbd723f84d
Wassim Kabalan <wastondev@gmail.com>
2025-11-21 14:27:57 +0100

    feat: extend map<->alm transforms and add spin APIs

--- jax_healpy/__init__.py
+++ jax_healpy/__init__.py
@@ -47,7 +47,21 @@ from .pixelfunc import (
     vec2pix,
     xyf2pix,
 )
-from .sphtfunc import alm2map, map2alm
+from .sphtfunc import (
+    alm2cl,
+    alm2map,
+    alm2map_spin,
+    almxfl,
+    anafast,
+    gauss_beam,
+    map2alm,
+    map2alm_spin,
+    pixwin,
+    smoothalm,
+    smoothing,
+    synalm,
+    synfast,
+)
 
 __all__ = [
     'pix2ang',
@@ -92,8 +106,19 @@ __all__ = [
     'query_disc',
     'estimate_disc_pixel_count',
     'estimate_disc_radius',
+    'alm2cl',
     'alm2map',
+    'alm2map_spin',
+    'almxfl',
+    'anafast',
+    'gauss_beam',
     'map2alm',
+    'map2alm_spin',
+    'pixwin',
+    'smoothalm',
+    'smoothing',
+    'synalm',
+    'synfast',
 ]
 
 _config.update('jax_enable_x64', True)
